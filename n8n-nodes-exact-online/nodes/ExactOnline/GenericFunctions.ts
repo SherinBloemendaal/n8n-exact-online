dcaab23d691bab2649abf935f27e228ef458fc24
Sherin Bloemendaal <sherin@bloemendaal.dev>
2025-01-23 11:35:59 +0100

    Wait 61 seconds to be sure

--- n8n-nodes-exact-online/nodes/ExactOnline/GenericFunctions.ts
+++ n8n-nodes-exact-online/nodes/ExactOnline/GenericFunctions.ts
@@ -74,7 +74,7 @@ export async function exactOnlineApiRequest(
 			response = await this.helpers.requestWithAuthentication.call(this, credentialType, options);
 		} catch (error) {
 			if (error.response && error.response.statusCode === 429) {
-				await new Promise((resolve) => setTimeout(resolve, 60000)); // Wait for 60 seconds before retrying
+				await new Promise((resolve) => setTimeout(resolve, 6000)); // Wait for 60 seconds before retrying
 				response = await this.helpers.requestWithAuthentication.call(this, credentialType, options);
 			} else {
 				throw error;
